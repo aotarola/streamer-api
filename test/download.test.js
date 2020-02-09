c2e00454ecbab8a2186b257b9cca0743ac0222d6
Andres Otarola <andres@otarola.me>
2020-02-08 16:13:56 -0800

    refactor: nit

--- test/download.test.js
+++ test/download.test.js
@@ -1,17 +1,20 @@
 'use strict';
 
 const assert = require('assertive');
-//move to before in mocha
-process.env.DOWNLOAD_PATH = '/tmp';
-const download = require('../lib/download');
+
 const nock = require('nock');
 const mockFs = require('mock-fs');
 const { promises: fs } = require('fs');
 
 const STUB_FILENAME = 'interesting-file.txt';
 
+let download;
+
 describe('download', () => {
   before(() => {
+    process.env.DOWNLOAD_PATH = '/tmp';
+    download = require('../lib/download');
+
     nock('http://test-domain.com')
       .get(`/${STUB_FILENAME}`)
       .reply(200, 'Interesting Content');
@@ -22,10 +25,12 @@ describe('download', () => {
       },
     });
   });
+
   after(() => {
     mockFs.restore();
   });
-  it('downloads a file', async () => {
+
+  it('should download a file', async () => {
     await download(new URL('http://test-domain.com/interesting-file.txt'));
     const fileContent = await fs.readFile(
       `${process.env.DOWNLOAD_PATH}/${STUB_FILENAME}`,
