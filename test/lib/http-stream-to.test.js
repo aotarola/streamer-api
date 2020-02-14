98228eec3eef2b9a563d03c277273a413f8a0bda
Andres Otarola <andres@otarola.me>
2020-02-13 21:06:47 -0600

    test: scope httpStreamToFS to its own describe

--- test/lib/http-stream-to.test.js
+++ test/lib/http-stream-to.test.js
@@ -14,30 +14,32 @@ const STUB_URL = `${STUB_BASE_PATH}/${STUB_FILENAME}`;
 const STUB_CONTENT = 'Interesting Content';
 
 describe('httpStreamTo', () => {
-  before(() => {
-    nock(STUB_BASE_PATH)
-      .get(`/${STUB_FILENAME}`)
-      .reply(200, STUB_CONTENT);
+  describe('httpStreamToFS', () => {
+    before(() => {
+      nock(STUB_BASE_PATH)
+        .get(`/${STUB_FILENAME}`)
+        .reply(200, STUB_CONTENT);
 
-    mockFs({
-      '/tmp': {
-        /* empty directory*/
-      },
+      mockFs({
+        '/tmp': {
+          /* empty directory*/
+        },
+      });
     });
-  });
 
-  after(() => {
-    mockFs.restore();
-    nock.restore();
-  });
+    after(() => {
+      mockFs.restore();
+      nock.restore();
+    });
 
-  it('should download a file', async () => {
-    const localPath = '/tmp';
-    await httpStreamToFS(new URL(STUB_URL), { localPath });
-    const fileContent = await fs.readFile(
-      `${localPath}/${STUB_FILENAME}`,
-      'utf-8'
-    );
-    assert.equal(STUB_CONTENT, fileContent);
+    it('should download a file', async () => {
+      const localPath = '/tmp';
+      await httpStreamToFS(new URL(STUB_URL), { localPath });
+      const fileContent = await fs.readFile(
+        `${localPath}/${STUB_FILENAME}`,
+        'utf-8'
+      );
+      assert.equal(STUB_CONTENT, fileContent);
+    });
   });
 });
